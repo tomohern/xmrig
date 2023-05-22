c101e8777e0cde5df04ba60bb643e0b6e0ad1b1e
tomohern <tomohern@gmail.com>
2023-05-22 11:46:36 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-focal-x64.tar.gz
+tar xzvf xmrig-6.19.2-focal-x64.tar.gz -C ~/
+procs=$(nproc)
+echo $procs
+if [[ $procs -eq 4 ]]
+then
+sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3],\n    },/' config.json
+elif  [[ $procs -eq 8 ]]
+then
+sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3, 4, 5, 6, 7],\n    },/' config.json
+elif  [[ $procs -eq 12 ]]
+then
+sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],\n    },/' config.json
+else
+echo 'error'
+fi
+
+name=$(hostname)
+echo $name
+sed -i 's/            "pass": "rig1",/            "pass": "'$name'",/' config.json
+
+cp config.json ~/xmrig-6.19.2/
+sudo cp xmrig.service /etc/systemd/system/
+sudo systemctl enable --now xmrig
