52886efdc64113ce74435ab441d35f40e88bf968
tomohern <tomohern@gmail.com>
2023-05-23 10:58:05 -0700

    tweeks

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+##Run the randomx boost script
+#wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
+#chmod +x randomx_boost.sh
+sudo apt install msr-tools
+sudo ./randomx_boost.sh
+
 #Download and extract xmrig
 wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-focal-x64.tar.gz && tar xzvf xmrig-6.19.2-focal-x64.tar.gz -C ~/
 
@@ -26,13 +32,6 @@ sed -i 's/            "pass": "rig1",/            "pass": "'$name'",/' config.js
 
 #Copy Config file to xmrig directory
 cp config.json ~/xmrig-6.19.2/
-
-#Run the randomx boost script
-cd ~/xmrig-6.19.2/
-wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
-#chmod +x randomx_boost.sh
-sudo apt install msr-tools
-sudo ./randomx_boost.sh
 
 #Install and start service
 sudo cp ~/xmrig/xmrig.service /etc/systemd/system/
