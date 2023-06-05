14c0b665b90aa989a3c96acb03c1e3d323f60629
tomohern <tomohern@gmail.com>
2023-06-05 10:52:31 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
 #chmod +x randomx_boost.sh
 sudo apt install msr-tools -y
-sudo ./randomx_boost.sh
+sudo ~/xmrig/randomx_boost.sh
 
 #Download and extract xmrig
 wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-focal-x64.tar.gz && tar xzvf xmrig-6.19.2-focal-x64.tar.gz -C ~/
@@ -14,13 +14,13 @@ procs=$(nproc)
 echo $procs
 if [[ $procs -eq 4 ]]
 then
-sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3],\n    },/' config.json
+sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3],\n    },/' ~/xmrig/config.json
 elif  [[ $procs -eq 8 ]]
 then
-sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3, 4, 5, 6, 7],\n    },/' config.json
+sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3, 4, 5, 6, 7],\n    },/' ~/xmrig/config.json
 elif  [[ $procs -eq 12 ]]
 then
-sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],\n    },/' config.json
+sed -i 's/    "cpu": true,/    "cpu": {\n        "rx": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],\n    },/' ~/xmrig/config.json
 else
 echo 'error'
 fi
@@ -28,14 +28,14 @@ fi
 #Update config file with correct hostname
 name=$(hostname)
 echo $name
-sed -i 's/            "pass": "rig1",/            "pass": "'$name'",/' config.json
+sed -i 's/            "pass": "rig1",/            "pass": "'$name'",/' ~/xmrig/config.json
 
 #Copy Config file to xmrig directory
-cp config.json ~/xmrig-6.19.2/
+cp ~/xmrig/config.json ~/xmrig-6.19.2/
 
 #Install and start service
 user=$(whoami)
-sed -i "s/user1/$user/g" xmrig.service
+sed -i "s/user1/$user/g" ~/xmrig/xmrig.service
 sudo cp ~/xmrig/xmrig.service /etc/systemd/system/
 sudo systemctl enable --now xmrig
 
